3cc11fcd664a7a27996087248e441b1833f8e8db
Rosydee <oomotoso5@Outlook.com>
2023-08-11 13:06:35 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
 
 for  (i = 0; i < 52; i++)
